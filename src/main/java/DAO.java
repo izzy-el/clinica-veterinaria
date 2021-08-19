0bc9a3c3c4597f042599d913b0b45450cc75118b
Izael <jv.izael@hotmail.com>
2021-08-19 11:48:45 -0300

    [feat] defined new methods on class DAO for creating and updating tables on the SQLite Database.

--- src/main/java/DAO.java
+++ src/main/java/DAO.java
@@ -22,10 +22,39 @@ public abstract class DAO {
         try {
             if(conn != null) {
                 conn.close();
-                System.out.println("Connection was succesfully closed.");
+                System.out.println("Connection was successfully closed.");
             }
         } catch (Exception e) {
             System.out.println("Exception: " + e);
         }
+    }
+
+//    Execute update statement
+//    Not sure what it does. Not going to use for now
+//    protected int executeUpdate(PreparedStatement query) throws SQLException {
+//        int update;
+//        update = query.executeUpdate();
+//        return update;
+//    }
+
+    //Create tables if they do not exist
+    protected final boolean createTable() {
+        PreparedStatement query;
+        try {
+            //TODO: Create the statements for the following tables/classes: Consult, Species, Exam, Treatment, Veterinarian.
+            //Animal
+            query = DAO.connect().prepareStatement("CREATE TABLE IF NOT EXISTS Animal(ID INTEGER PRIMARY KEY, Name VARCHAR(255), Age INTEGER, Gender INTEGER)");
+            query.executeUpdate();
+
+            //Client
+            query = DAO.connect().prepareStatement("CREATE TABLE IF NOT EXISTS Client(ID INTEGER PRIMARY KEY, Name VARCHAR(255), Address VARCHAR(255), Phone VARCHAR(255))");
+            query.executeUpdate();
+
+            return true;
+        } catch(Exception e) {
+            System.out.println("Exception: " + e);
+        }
+
+        return false;
     }
 }
