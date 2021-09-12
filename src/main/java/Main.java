c91b3846df782e219c6130ccfa59a94eb3e9f9a6
Izael <jv.izael@hotmail.com>
2021-09-12 18:45:29 -0300

    [refactor] cleaning the Main.java file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,30 +2,6 @@ import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
-        TreatmentDAO treatmentDAO;
-        treatmentDAO = TreatmentDAO.getInstance();
-
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-        Treatment treatment = treatmentDAO.retrieveById(1);
-        System.out.println(treatment);
-
-//        treatmentDAO.create(1, "Vaccine", sdf.parse("26/01/2002"), sdf.parse("27/01/2002"), true);
-//        Treatment treatment = treatmentDAO.retrieveById(3);
-//        System.out.println(treatment);
-
-//        Treatment treatment = treatmentDAO.retrieveAll().get(0);
-
-//        ClientDAO clientDAO;
-//        AnimalDAO animalDAO;
-
-//        clientDAO = ClientDAO.getInstance();
-//        animalDAO = AnimalDAO.getInstance();
-//
-//        Client client = clientDAO.retrieveById(1);
-//        System.out.println(client);
-//        client.addAnimal(animalDAO.retrieveByClientId(client));
-//
-//        System.out.println(client.toString());
     }
 }
