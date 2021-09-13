3292f5ac8eda6bed8178b3562cbd4b5a95b230e2
Izael <jv.izael@hotmail.com>
2021-09-13 15:12:29 -0300

    [fix] implemented some cases to show that the program is working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,30 @@
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        AnimalDAO animalDAO;
+        animalDAO = AnimalDAO.getInstance();
+        AppointmentDAO appointmentDAO;
+        appointmentDAO = AppointmentDAO.getInstance();
+        ClientDAO clientDAO;
+        clientDAO = ClientDAO.getInstance();
+        ExamDAO examDAO;
+        examDAO = ExamDAO.getInstance();
+        SpeciesDAO speciesDAO;
+        speciesDAO = SpeciesDAO.getInstance();
+        TreatmentDAO treatmentDAO;
+        treatmentDAO = TreatmentDAO.getInstance();
+        VeterinarianDAO veterinarianDAO;
+        veterinarianDAO = VeterinarianDAO.getInstance();
+
+        clientDAO.create("Amy", "Rua Alberta", "523667895", "256032", "amy@gmail.com");
+        animalDAO.create("Spartan", 3, "Male", 3);
+        veterinarianDAO.create("Scott", "Rua das Americas", "526227485", "scot@gmail.com");
+        speciesDAO.create("Horse");
+        treatmentDAO.create(6, "Surgery", sdf.parse("12/09/2021"), sdf.parse("12/09/2021"), false);
+        appointmentDAO.create(sdf.parse("12/09/2021"), "15:00", "None", 6, 1, 1, false);
+        examDAO.create("Surgery Checkup", 1);
     }
 }
