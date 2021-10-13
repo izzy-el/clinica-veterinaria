9ec6afaeff4a24783c1923d4d92637d7ea1e4623
Izael <jv.izael@hotmail.com>
2021-10-13 10:12:17 -0300

    [refactor] Changing the entire project structure due to be using Netbeans now.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ import java.text.SimpleDateFormat;
 public class Main {
     public static void main(String[] args) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        System.out.println("It is working.");
     }
 }
--- src/main/java/model/Animal.java
+++ src/main/java/model/Animal.java
@@ -1,5 +1,14 @@
 package model;
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author izael
+ */
 public class Animal {
     //Attributes
     private int id;
